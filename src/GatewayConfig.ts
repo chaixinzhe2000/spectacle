afa697443f37863e1187854077fb0761e86b6b4f
Xinzhe Chai <xinzhe_chai@brown.edu>
2020-12-04 22:37:13 -0500

    completed

--- src/GatewayConfig.ts
+++ src/GatewayConfig.ts
@@ -1,6 +1,6 @@
 // Add your backend endpoints, make sure the endpoints DO NOT end with a '/' the endpoint should end with '.com'
 // TODO
-export const LinkGatewayEndpoint = ''
-export const AnchorGatewayEndpoint = ''
-export const NodeGatewayEndpoint = ''
+export const LinkGatewayEndpoint = 'https://link-server-backend.herokuapp.com'
+export const AnchorGatewayEndpoint = 'https://anchor-server-backend.herokuapp.com'
+export const NodeGatewayEndpoint = 'https://quiet-journey-87714.herokuapp.com'
 export const ImmutableGridGatewayEndpoint = 'https://stark-savannah-73845.herokuapp.com'
