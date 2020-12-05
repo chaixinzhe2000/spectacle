58bafa698c7cd3252b932cc4843c599fb14e75d7
elliotjinoohong <60581173+elliotjinoohong@users.noreply.github.com>
2020-12-05 04:25:12 -0500

    updated helper.ts

--- src/database/helpers.ts
+++ src/database/helpers.ts
@@ -14,17 +14,17 @@ export interface IMongoLink {
 	createdAt?: Date;
 }
 
-function isLink(link: any): boolean {
+function isLink(link: ILink): boolean {
 	try {
-		let fieldCount: number = 0
-		if (link.srcAnchorId !== undefined && typeof link.srcAnchorId === "string" && link.srcAnchorId.length > 0)
+        let fieldCount: number = 0
+		if (link.srcAnchorId !== undefined && link.srcNodeId !== null && typeof link.srcAnchorId === "string" && link.srcAnchorId.length > 0)
 			fieldCount++
-		if (link.destAnchorId !== undefined && typeof link.srcAnchorId === "string" && link.srcAnchorId.length > 0)
+		if (link.destAnchorId !== undefined && link.srcNodeId !== null && typeof link.srcAnchorId === "string" && link.srcAnchorId.length > 0)
 			fieldCount++
-		if (link.srcNodeId !== undefined && typeof link.srcAnchorId === "string" && link.srcAnchorId.length > 0)
+		if (link.srcNodeId !== undefined && link.srcNodeId !== null && typeof link.srcAnchorId === "string" && link.srcAnchorId.length > 0)
 			fieldCount++
-		if (link.destNodeId !== undefined && typeof link.srcAnchorId === "string" && link.srcAnchorId.length > 0)
-			fieldCount++
+		if (link.destNodeId !== undefined && link.srcNodeId !== null && typeof link.srcAnchorId === "string" && link.srcAnchorId.length > 0)
+            fieldCount++
 		if (fieldCount === 2)
 			return true
 		return false
@@ -34,6 +34,11 @@ function isLink(link: any): boolean {
 }
 export function getMongoLink(link: ILink): IServiceResponse<IMongoLink> {
 	try {
+        if (!isLink(link)){
+            return failureServiceResponse(
+                "Input to getMongoLink is not valid."
+            );
+        }
 		let mongolink: IMongoLink = {
 			_id: link.linkId.toLocaleLowerCase(),
 			srcAnchorId: link.srcAnchorId,
@@ -41,10 +46,11 @@ export function getMongoLink(link: ILink): IServiceResponse<IMongoLink> {
 			srcNodeId: link.srcNodeId,
 			destNodeId: link.destNodeId,
 			createdAt: new Date(),
-		};
-		if (tryGetLink(mongolink).success && isLink(link)) {
+        };
+        const tryMongoLinkResponse = tryGetLink(mongolink)
+		if (tryGetLink(mongolink).success) {
 			return successfulServiceResponse(mongolink);
-		}
+        }
 		return failureServiceResponse(
 			"Failed to parse ILink into IMongoNode, verify that the ILink passed in is valid."
 		);
@@ -57,13 +63,13 @@ export function getMongoLink(link: ILink): IServiceResponse<IMongoLink> {
 
 export function tryGetLink(mongoLink: IMongoLink): IServiceResponse<ILink> {
 	if (mongoLink._id !== undefined && typeof mongoLink._id === "string" &&
-		mongoLink._id.length > 0 && isLink(mongoLink))
+        mongoLink._id.length > 0)
 		return successfulServiceResponse({
 			linkId: mongoLink._id,
 			srcAnchorId: mongoLink.srcAnchorId,
 			destAnchorId: mongoLink.destAnchorId,
 			srcNodeId: mongoLink.srcNodeId,
 			destNodeId: mongoLink.destNodeId,
-		});
-	return failureServiceResponse("Invalid node");
+        });
+	return failureServiceResponse("Invalid link");
 }
