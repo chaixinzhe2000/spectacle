1a43da548688ec5c3a699a03a5e9de1d41d4cb38
Xinzhe Chai <xinzhe_chai@brown.edu>
2020-12-05 14:49:04 -0500

    the interface is somehow finished

--- src/NodeDisplay/ReactPlayer.tsx
+++ src/NodeDisplay/ReactPlayer.tsx
@@ -8,10 +8,14 @@ export default class ReactPlayerWrapper extends React.Component {
 	render() {
 		return (
 			<div>
+				<div className="comment-title">Lecture Content</div>
 				<div className="wrapper">
-					<div className="comment-title">Lecture Content</div>
-					<ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+					<ReactPlayer url='https://www.youtube.com/watch?v=6KP7tu3JLSM' />
 				</div>
+				<div className="comment-title">Related Nodes</div>
+				<div className="comment">Media Node: Andy's Lecture on Hypertext System</div>
+				<div className="comment">Essay Submission: Which hypertext system should I use?</div>
+				<div className="comment">PDF: new PDF</div>
 			</div>
 		);
 	}
