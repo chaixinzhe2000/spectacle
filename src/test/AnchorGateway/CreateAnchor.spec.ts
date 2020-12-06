2b9aa29226c86e75a8f00efe952053910d5f10e5
elliotjinoohong <60581173+elliotjinoohong@users.noreply.github.com>
2020-12-06 15:04:30 -0500

    kira please finish createAnchor (anchor gateway) testing

--- src/test/AnchorGateway/CreateAnchor.spec.ts
+++ src/test/AnchorGateway/CreateAnchor.spec.ts
@@ -34,13 +34,13 @@ describe('Gateway Test: Create Anchor', () => {
 			createdAt: new Date()
 		}
 
-		const getResponse = await DatabaseConnection.findAnchor(validanchor.anchorId)
+		const getResponse = await anchorGateway.getAnchor(validanchor.anchorId)
 		expect(getResponse.success).toBeFalsy()
 
 		const response = await anchorGateway.createAnchor(validanchor)
 		expect(response.success).toBeTruthy()
 
-		const getResponse2 = await DatabaseConnection.findAnchor(validanchor.anchorId)
+		const getResponse2 = await anchorGateway.getAnchor(validanchor.anchorId)
 		expect(getResponse2.success).toBeTruthy()
         expect(getResponse2.payload.contentList).toEqual(["Hello Chai Hello Chai"])
         expect(getResponse2.payload.authorList).toEqual(["Chai"])
@@ -59,39 +59,81 @@ describe('Gateway Test: Create Anchor', () => {
 			createdAt: new Date()
 		}
 
-		const getResponse = await DatabaseConnection.findAnchor(validanchor.anchorId)
+		const getResponse = await anchorGateway.getAnchor(validanchor.anchorId)
 		expect(getResponse.success).toBeFalsy()
 
 		const response = await anchorGateway.createAnchor(validanchor)
 		expect(response.success).toBeTruthy()
 
-		const getResponse2 = await DatabaseConnection.findAnchor(validanchor.anchorId)
+		const getResponse2 = await anchorGateway.getAnchor(validanchor.anchorId)
 		expect(getResponse2.success).toBeTruthy()
         expect(getResponse2.payload.contentList).toEqual(["I like this a lot mmm!"])
         expect(getResponse2.payload.authorList).toEqual(["Jinoo"])
 		expect(getResponse2.payload.type).toBe("media")
 
-		const validanchor2: IAnchor = {
+		const invalidanchor: IAnchor = {
 			nodeId: 'id2',
 			anchorId: 'anchor.id2',
-			contentList: ["I like this a lot mmm!"],
-            authorList: ["Jinoo"],
+			contentList: ["invalid content"],
+            authorList: ["invalid author"],
 			type: "media",
 			createdAt: new Date()
 		}
 
-		const response2 = await anchorGateway.createAnchor(validanchor2)
+		const response2 = await anchorGateway.createAnchor(invalidanchor)
 		expect(response2.success).toBeFalsy()
 
-		const getResponse3 = await DatabaseConnection.findAnchor(validanchor.anchorId)
+		const getResponse3 = await anchorGateway.getAnchor(invalidanchor.anchorId)
 		expect(getResponse3.success).toBeTruthy()
-        expect(getResponse2.payload.contentList).toEqual(["I like this a lot mmm!"])
-        expect(getResponse2.payload.authorList).toEqual(["Jinoo"])
-		expect(getResponse2.payload.type).toBe("media")
+        expect(getResponse3.payload.contentList).toEqual(["I like this a lot mmm!"])
+        expect(getResponse3.payload.authorList).toEqual(["Jinoo"])
+		expect(getResponse3.payload.type).toBe("media")
 		done()
 	})
 
-	test("fails to create when anchorId is null", async done => {
+	
+    
+    // if nodeId is wrong type
+    // if nodeId is ''
+    // if nodeId is null
+    test("fails to create anchor when nodeId wrong", async done => {
+		const invalidanchor: any = {
+			nodeId: 1,
+			anchorId: 'a',
+            contentList: ["content a"],
+            authorList: ["author a"],
+			type: "media",
+			createdAt: new Date()
+		}
+		const response = await anchorGateway.createAnchor(invalidanchor)
+        expect(response.success).toBeFalsy()
+
+        const invalidanchor2: any = {
+			nodeId: '',
+			anchorId: 'a',
+            contentList: ["content a"],
+            authorList: ["author a"],
+			type: "media",
+			createdAt: new Date()
+		}
+		const response2 = await anchorGateway.createAnchor(invalidanchor2)
+        expect(response2.success).toBeFalsy()
+        
+        const invalidanchor3: any = {
+			nodeId: null,
+			anchorId: 'a',
+            contentList: ["content a"],
+            authorList: ["author a"],
+			type: "media",
+			createdAt: new Date()
+		}
+		const response3 = await anchorGateway.createAnchor(invalidanchor3)
+		expect(response3.success).toBeFalsy()
+		done()
+    })
+
+    // if anchorId is wrong type
+    test("fails to create when anchorId is null", async done => {
 		const invalidanchor: any = {
 			nodeId: 'id4',
 			anchorId: null,
@@ -105,6 +147,7 @@ describe('Gateway Test: Create Anchor', () => {
 		done()
 	})
 
+    // if anchorId is null
 	test("fails to create anchor when anchorId is number", async done => {
 		const invalidanchor: any = {
 			nodeId: 'id4',
@@ -118,16 +161,64 @@ describe('Gateway Test: Create Anchor', () => {
 		done()
 	})
 
+    // if anchorId is ''
 	test("fails to create anchor when anchorId is empty", async done => {
 		const invalidanchor: any = {
 			nodeId: 'id4',
 			anchorId: '',
-			content: "I like this a lot!",
+			content: ["I like this a lot!"],
 			type: "media",
 			createdAt: new Date()
 		}
 		const response = await anchorGateway.createAnchor(invalidanchor)
 		expect(response.success).toBeFalsy()
 		done()
-	})
+    })
+    
+
+    // KIRA START HERE TO TEST TY (STUFF BELOW HERE IS WRONG)
+    // if type is ''
+    // if type is null
+    // if type is not a NodeType
+    test("fails to create anchor when type is wrong", async done => {
+		const invalidanchor: any = {
+			nodeId: "hi",
+			anchorId: 'a',
+            contentList: ["content a"],
+            authorList: ["author a"],
+			type: "media",
+			createdAt: new Date()
+		}
+		const response = await anchorGateway.createAnchor(invalidanchor)
+        expect(response.success).toBeFalsy()
+
+        const invalidanchor2: any = {
+			nodeId: '',
+			anchorId: 'a',
+            contentList: ["content a"],
+            authorList: ["author a"],
+			type: "media",
+			createdAt: new Date()
+		}
+		const response2 = await anchorGateway.createAnchor(invalidanchor2)
+        expect(response2.success).toBeFalsy()
+        
+        const invalidanchor3: any = {
+			nodeId: null,
+			anchorId: 'a',
+            contentList: ["content a"],
+            authorList: ["author a"],
+			type: "media",
+			createdAt: new Date()
+		}
+		const response3 = await anchorGateway.createAnchor(invalidanchor3)
+		expect(response3.success).toBeFalsy()
+		done()
+    })
+
+    // for the next four, just enter a string (for "not a list") and empty list (for "not an empty list") and ensure createAnchor gives falsy
+    // if contentList is not a list
+    // if contentList is an empty list
+    // if authorList is not a list
+    // if authorList is an empty list
 })
