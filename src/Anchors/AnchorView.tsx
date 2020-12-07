5b72f1647f5537cb01dd9702cdc15006870a469d
KiraKC <>
2020-12-07 03:12:14 -0500

    anchor view and container somewhat working
    missing timestamp and linking

--- src/Anchors/AnchorView.tsx
+++ src/Anchors/AnchorView.tsx
@@ -1,12 +1,12 @@
-
-
-import { H5 } from '@blueprintjs/core';
+import { H5, Button, Card, Elevation, Divider } from '@blueprintjs/core';
 import { failureServiceResponse, IAnchor, ILink, INode, IServiceResponse } from 'spectacle-interfaces';
 import React, { useState } from 'react';
 import { Collapse } from 'antd';
 import { Accordion, Icon } from 'semantic-ui-react'
 import LinkView from '../Links/LinkView';
 import LinkContainer from '../Links/LinkContainer';
+import MediaAnchorGateway from '../Gateways/Media/MediaAnchorGateway';
+
 
 const { Panel } = Collapse;
 
@@ -16,39 +16,77 @@ interface AnchorViewProps {
     anchor?: IAnchor
     setAnchor: (anchor: IAnchor) => void
     setPreviewAnchor: (anchor: IAnchor) => void
-    linkMap: {[anchorId: string] : ILink[]}
+    linkMap: { [anchorId: string]: ILink[] }
     canManageLinks: boolean
+    mediaAnchorTimeStamps: number[]
 }
 
 function AnchorView(props: AnchorViewProps): JSX.Element {
-    const { anchors, anchor, setAnchor, getNode, setPreviewAnchor, linkMap, canManageLinks } = props
-    
-    const activeIndex = anchor ? anchors.findIndex(anc => anc.anchorId === anchor.anchorId) : -1
-
-    if (anchors.length)
-        return (
-            <Accordion onMouseLeave={() => setPreviewAnchor(null)} styled fluid>
-                {anchors.map((a, index) => <div key={a.anchorId}>
-                        <Accordion.Title
-                            active={activeIndex === index}
-                            index={index}
-                            onClick={e => setAnchor(a)}
-                            onMouseEnter={() => setPreviewAnchor(a)}
-                        >
-                        <Icon name='dropdown' />
-                        {"CHANGE THIS LATER IN ANCHORVIEW"} ({linkMap[a.anchorId] ? linkMap[a.anchorId].length : 0} {linkMap[a.anchorId] ? linkMap[a.anchorId].length === 1 ? 'Link' : 'Links' : 'Links' })
-                    </Accordion.Title>
-                    {
-                        canManageLinks && <Accordion.Content active={activeIndex === index}>
-                            <LinkContainer anchor={a} />
-                        </Accordion.Content>
-                    }
+    const { anchors, anchor, setAnchor, getNode, setPreviewAnchor, linkMap, canManageLinks, mediaAnchorTimeStamps } = props
+    if (anchors.length) {
+        // media annotations
+        if (anchors[0].type === "media"){
+            return (
+                <div>
+                    {anchors.map((a, index) =>
+                    <div key={a.anchorId}>
+                        <Card interactive={true} elevation={Elevation.TWO}>
+                            <h5><a href="#">{mediaAnchorTimeStamps[index]}</a></h5>
+                            {anchors[index].contentList.map((c, cIndex) =>
+                                <div key={cIndex}>
+                                    <p>{c}</p>
+                                    <Divider />
+                                </div>
+                            )}
+                        </Card>
                     </div>
-                )}
-        </Accordion>
-    )
-    else
+                    )}
+                </div>
+            )
+        }
+        // Non-media annotations
+        else {
+            return (
+                <div>
+                    {anchors.map((a, index) =>
+                    <div key={a.anchorId}>
+                        <Card interactive={true} elevation={Elevation.TWO}>
+                            <h5><a href="#">this is non-media annotation</a></h5>
+                            <p>Card content</p>
+                        </Card>
+                    </div>
+                    )}
+                </div>
+            )
+        }
+        
+    } else {
         return null
+    }
+    // if (anchors.length)
+    //     return (
+    //         <Accordion onMouseLeave={() => setPreviewAnchor(null)} styled fluid>
+    //             {anchors.map((a, index) => <div key={a.anchorId}>
+    //                 <Accordion.Title
+    //                     active={activeIndex === index}
+    //                     index={index}
+    //                     onClick={e => setAnchor(a)}
+    //                     onMouseEnter={() => setPreviewAnchor(a)}
+    //                 >
+    //                     <Icon name='dropdown' />
+    //                     {a.contentList[0]} ({linkMap[a.anchorId] ? linkMap[a.anchorId].length : 0} {linkMap[a.anchorId] ? linkMap[a.anchorId].length === 1 ? 'Link' : 'Links' : 'Links'})
+    //                 </Accordion.Title>
+    //                 {
+    //                     canManageLinks && <Accordion.Content active={activeIndex === index}>
+    //                         <LinkContainer anchor={a} />
+    //                     </Accordion.Content>
+    //                 }
+    //             </div>
+    //             )}
+    //         </Accordion>
+    //     )
+    // else
+    //     return null
 }
 
 export default AnchorView;
