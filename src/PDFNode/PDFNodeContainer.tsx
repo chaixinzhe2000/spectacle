8210a3ce58b2860ff34db3f21370ad70bc9b0402
elliotjinoohong <60581173+elliotjinoohong@users.noreply.github.com>
2020-12-09 08:57:19 -0500

    pdf basic done

--- src/PDFNode/PDFNodeContainer.tsx
+++ src/PDFNode/PDFNodeContainer.tsx
@@ -9,27 +9,27 @@ import PDFNodeGateway from '../Gateways/PDFGateway';
 
 interface PDFContainerProps {
     node: INode
-    anchorId: string
-    anchorIds: string[]
     newPDFAnchorModal: boolean
     setNewPDFAnchorModal: any
 }
 
 function ImmutableTextContainer(props: PDFContainerProps): JSX.Element {
-	const { node, anchorId, anchorIds, newPDFAnchorModal, setNewPDFAnchorModal } = props
+	const { node, newPDFAnchorModal, setNewPDFAnchorModal } = props
 
     const { isLoading, data, error } = useQuery([node.nodeId, node.nodeType], PDFNodeGateway.getNode)
 
 
     const [createNode] = useMutation(PDFNodeGateway.createNode, {
 		onSuccess: () => queryCache.invalidateQueries([node.nodeId, node.nodeType])
-	})
+    })
+    
+    if (isLoading) return <Spinner />
+
+	if (error) return <div> {'An error has occurred: ' + error} </div>
 
 	return (<div>
 		<PDFWithAnchorsContainer
             node={data?.payload}
-            anchorId={anchorId}
-            anchorIds={anchorIds}
             createNode={PDFUrl => createNode({
 				nodeId: node.nodeId,
 				pdfUrl: PDFUrl
